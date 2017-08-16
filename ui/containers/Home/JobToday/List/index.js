306a247bdd02917fad49cb17c60cc9477da44774
Dungnt <trandung1128@gmail.com>
2017-08-16 11:53:43 +0700

    get day: list job by id

--- ui/containers/Home/JobToday/List/index.js
+++ ui/containers/Home/JobToday/List/index.js
@@ -22,11 +22,12 @@ import Calendar from '~/ui/components/Calendar';
 
 import * as jobActions from '~/store/actions/job'
 import * as jobSelectors from '~/store/selectors/job'
+import * as contactActions from '~/store/actions/contact'
 const accessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik9EZzFPVVF4UmpZelJEZzVSakUzT0RBME5UUkZRa1pHUkRJd016ZERPRFl4TmpRd09UaEdSUSJ9.eyJpc3MiOiJodHRwczovL3R1YW5wbDEuYXUuYXV0aDAuY29tLyIsInN1YiI6ImVvc29UR3FCMHZwNWlsS1dWMGcxclZmaVBFMGRaWnVGQGNsaWVudHMiLCJhdWQiOiJodHRwczovL3R1YW5wbDF0ZXN0IiwiZXhwIjoxNTExMDg3NDQ0LCJpYXQiOjE1MDI0NDc0NDQsInNjb3BlIjoiIn0.U3xQQLeGTFuzr-37PXefhZnynHWYUx7Ow_SuBfb8FM2S3cxAQdk6WN14bPKqSKaAsbMU7Sd6VsvTFDtlSRrkDmghfNNIQ7eTD8qECZ6N94XePH-oggOM7PDUVsWzTT5t5279w-8PFc5NjByPiptu-hvAV2JAR0tJd_UDJHF-tArnYeq99v_bftkdhngd_JblRJBC6oDqaAGPaAQa4SCL0aG3WxUXVz1CeLywyKUBYVE88RWC-GWlnwozBcegqku5BRP4zzlJmY3Xw73Bdj8zEt5aQtl_rc3EaG2mwFtUMokBNxUAqzHtG3WCFgCxb2463EvyCqJQHlwAFnzGYFwqDg'
 
 @connect(state => ({
     listStatus: jobSelectors.getStatusJobList(state)
-}), { ...jobActions })
+}), { ...jobActions, ...contactActions })
 
 export default class extends Component {
     constructor(props) {
@@ -63,12 +64,13 @@ export default class extends Component {
     componentDidMount() {
         console.log(this.renderDate(new Date()))
 
-        this.props.getJobByDate(this.state.listByDate, accessToken, (error, data) => {
+        this.props.getJobByDate(this.renderDate(new Date()), accessToken, (error, data) => {
             this.setState({
                 dataSource: data.JobListItemObjects,
             })
             console.log(data)
         })
+
     }
 
     updateList(date) {
@@ -160,6 +162,9 @@ export default class extends Component {
     }
  
     render() {
+        this.props.getContactReferece(accessToken, (error, data) => {
+            console.log(data)
+        })
 
         // const dayNow = this.renderDate();
         return (
