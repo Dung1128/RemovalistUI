88a395530032a73e62869199cbe7e2a9d417e00c
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-08-17 15:18:36 +0700

    fix scroll

--- ui/containers/Home/JobToday/List/index.js
+++ ui/containers/Home/JobToday/List/index.js
@@ -19,7 +19,7 @@ import material from '~/theme/variables/material'
 import TitleItem from '~/ui/components/TitleItem';
 import ListView from './components/ListView';
 import Calendar from '~/ui/components/Calendar';
-
+import moment from 'moment'
 import * as jobActions from '~/store/actions/job'
 import * as jobSelectors from '~/store/selectors/job'
 const accessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik9EZzFPVVF4UmpZelJEZzVSakUzT0RBME5UUkZRa1pHUkRJd016ZERPRFl4TmpRd09UaEdSUSJ9.eyJpc3MiOiJodHRwczovL3R1YW5wbDEuYXUuYXV0aDAuY29tLyIsInN1YiI6ImVvc29UR3FCMHZwNWlsS1dWMGcxclZmaVBFMGRaWnVGQGNsaWVudHMiLCJhdWQiOiJodHRwczovL3R1YW5wbDF0ZXN0IiwiZXhwIjoxNTExMDg3NDQ0LCJpYXQiOjE1MDI0NDc0NDQsInNjb3BlIjoiIn0.U3xQQLeGTFuzr-37PXefhZnynHWYUx7Ow_SuBfb8FM2S3cxAQdk6WN14bPKqSKaAsbMU7Sd6VsvTFDtlSRrkDmghfNNIQ7eTD8qECZ6N94XePH-oggOM7PDUVsWzTT5t5279w-8PFc5NjByPiptu-hvAV2JAR0tJd_UDJHF-tArnYeq99v_bftkdhngd_JblRJBC6oDqaAGPaAQa4SCL0aG3WxUXVz1CeLywyKUBYVE88RWC-GWlnwozBcegqku5BRP4zzlJmY3Xw73Bdj8zEt5aQtl_rc3EaG2mwFtUMokBNxUAqzHtG3WCFgCxb2463EvyCqJQHlwAFnzGYFwqDg'
@@ -38,7 +38,7 @@ export default class extends Component {
             dataSource: [],
             listByDate: 'init'
         };
-        this.date = null
+        this.date = new Date()
     }
 
     renderStatus(id) {
@@ -55,16 +55,16 @@ export default class extends Component {
 
     componentWillMount() {
         this.setState({
-            listByDate: this.renderDate(new Date())
+            listByDate: this.renderDate(this.date)
         })
         InteractionManager.runAfterInteractions(() => {
             this.setState({ ready: true });
         });
     }
     componentDidMount() {
-        console.log(this.renderDate(new Date()))
+        console.log(this.renderDate(this.date))
 
-        this.props.getJobByDate(this.renderDate(new Date()), accessToken, (error, data) => {
+        this.props.getJobByDate(this.renderDate(this.date), accessToken, (error, data) => {
             this.setState({
                 dataSource: data.JobListItemObjects,
                 
@@ -178,10 +178,11 @@ export default class extends Component {
         return (
             <View style={{ flex: 1 }}>
                 <Calendar style={{ width: '100%', marginHorizontal: 10 }}
+                currentMonth={this.date}
                 scrollEnabled={true}
                  onDateSelect={date => this.onDateSelect(date)} />
                 {
-                    this.state.listByDate == this.renderDate(new Date()) ? <TitleItem title='Today' /> : <TitleItem title={this.state.dateTitle} />
+                    this.state.listByDate == this.renderDate(this.date) ? <TitleItem title='Today' /> : <TitleItem title={this.state.dateTitle} />
                 }
                 {!this.state.ready && <Spinner color={material.redColor} style={{ marginTop: '50%' }} />}
                 {
